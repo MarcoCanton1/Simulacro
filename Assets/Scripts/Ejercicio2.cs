232a562b0607a6290f0722bfa8653798d196958d
Marco Canton <67104790+MarcoCanton1@users.noreply.github.com>
2022-04-07 15:15:30 -0300

    Ejercicios terminados

---
+++ Assets/Scripts/Ejercicio2.cs
@@ -0,0 +1,37 @@
+﻿//2.Crear las variables necesarias para ingresar por el inspector el precio de 3 productos y un monto de dinero disponible.
+//Comunicar si la suma de los mismos supero o no el monto de dinero disponible.Luego indicar cuanto dinero sobra o falta
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2 : MonoBehaviour
+{
+    public float prod1;
+    public float prod2;
+    public float prod3;
+    public float montoD;
+    float total;
+    float resto;
+    float faltante;
+    void Start()
+    {
+        total = prod1 + prod2 + prod3;
+        if (total <= montoD)
+        {
+            resto = montoD - total;
+            Debug.Log("El total no supera el monto de dinero, ah sobrado $" + resto);
+        }
+        else if (total > montoD)
+        {
+            faltante = total - montoD;
+            Debug.Log("El total supera el monto de dinero, por lo tanto faltan $" + faltante);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
--- Assets/Scripts/Script.cs
+++ Assets/Scripts/Script.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
+﻿//1.Crear un programa que permita ingresar por inspector dos valores enteros y responda si son iguales o no lo son
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Script : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int num1;
+    public int num2;
     void Start()
     {
-        
+        if (num1 == num2)
+        {
+            Debug.Log("Los numeros son iguales");
+        }
+        else if (num1 != num2)
+        {
+            Debug.Log("Los numeros son distintos");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
